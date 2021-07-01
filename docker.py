cf951250efc72576d45d106ea5829ef1ae7c5145
vedant011 <64039474+vedant011@users.noreply.github.com>
2021-07-01 20:07:12 +0530

    Create docker.py
    2

---
+++ docker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+print("content-type:text/html \n")
+print()
+
+import cgi
+import subprocess
+
+y = cgi.FieldStorage()
+cmd = y.getvalue("x")
+
+
+output = subprocess.getoutput("sudo "+cmd)
+print(output)
